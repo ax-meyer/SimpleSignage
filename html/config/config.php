3392533147bbdb6294ce08568ada2672ec432523
Alexander Meyer <github@meyerweb.eu>
2019-05-08 18:49:49 +0200

    Initial commit with first working version.

---
+++ html/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'sitename' => 'K2 Signage',
+	'siteheader' => 'Kafarna:um Werbesystem',
+	'dbpath' => 'signage.db',
+	'imagepath' => 'images',
+	'imagedisplaytime' => 10,
+	'reloadtime' => 3600,
+	'timezone' => 'Europe/Berlin'
+);
