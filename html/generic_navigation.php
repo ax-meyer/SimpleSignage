12c0f79584349ecc52334b81e5b94baa02d33f9b
ax-meyer <32029723+ax-meyer@users.noreply.github.com>
2021-09-19 21:29:36 +0200

    Revert "Recreate database model"

---
+++ html/generic_navigation.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php"><?php echo($configs['siteheader']); ?></a>
+        </div>
+        
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        
+        <!-- Top Navigation: Right Menu -->
+        <ul class="nav navbar-right navbar-top-links">
+            <li class="dropdown">
+                
+            </li>
+        </ul>
+
+        <!-- Sidebar -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+
+                <ul class="nav" id="side-menu">
+                    <li>
+                        <a href="images.php"><i class="fa fa-dashboard fa-fw"></i> Images </a>
+                    </li>
+					<li>
+                        <a href="devices.php"><i class="fa fa-dashboard fa-fw"></i> Devices </a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+  </nav>
